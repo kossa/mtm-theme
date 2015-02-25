75dcec6b2c87aad45eb36db59c0ee791c3d877ae
kossa <hadjikouceyla@gmail.com>
2015-02-25 18:08:32 +0100

    Home - Slider Done

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@ session_start();
 
 // Add Navigation menu
 function mtm_register_theme_menu() {
-    register_nav_menu( 'primary', 'Main Navigation Menu' );
+        register_nav_menu( 'primary', 'Main Navigation Menu' );
 }
 add_action( 'init', 'mtm_register_theme_menu' );
 
@@ -23,24 +23,24 @@ add_action( 'init', 'mtm_register_theme_menu' );
 */
 function mtm_widgets_init() {
 
-    // Home-page: Bienvenu
-    register_sidebar( array(
-        'name'          => __( 'HomePage : Bienvenu', 'mtm' ),
-        'id'            => 'home-bienvenue',
-        'description'   => __( 'Le message Bienvenue sur la page d\'accueil.', 'mtm' ),
-        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</aside>',
-    ) );
+        // Home-page: Bienvenu
+        register_sidebar( array(
+                'name'          => __( 'HomePage : Bienvenu', 'mtm' ),
+                'id'            => 'home-bienvenue',
+                'description'   => __( 'Le message Bienvenue sur la page d\'accueil.', 'mtm' ),
+                'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+                'after_widget'  => '</aside>',
+        ) );
 
-    // What We Do: Bienvenu
-    register_sidebar( array(
-        'name'          => __( 'what-we-do : what-think', 'mtm' ),
-        'id'            => 'what-think',
-        'description'   => __( 'what-think position', 'mtm' ),
-        'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</aside>',
-    ) );
-    
+        // What We Do: Bienvenu
+        register_sidebar( array(
+                'name'          => __( 'what-we-do : what-think', 'mtm' ),
+                'id'            => 'what-think',
+                'description'   => __( 'what-think position', 'mtm' ),
+                'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+                'after_widget'  => '</aside>',
+        ) );
+        
 }
 add_action( 'widgets_init', 'mtm_widgets_init' );
 
@@ -67,12 +67,12 @@ remove_filter( 'the_excerpt', 'wpautop' );
 |------------------------------------------------------------------------------------
 */
 function add_class_body(){
-    $classes = array();
-    if (is_front_page()) {
-         array_push($classes, 'home-page');
-    }
+        $classes = array();
+        if (is_front_page()) {
+                 array_push($classes, 'home-page');
+        }
 
-    return $classes;
+        return $classes;
 }  
 
 
@@ -83,15 +83,15 @@ function add_class_body(){
 */
 function GetPosts($type, $options=[])
 {
-  $args = array (
-    'post_type'        => $type,
-    'post_status'      => 'publish',
-    'posts_per_page'   => -1,
-    'caller_get_posts' => 1,
-  );
+    $args = array (
+        'post_type'        => $type,
+        'post_status'      => 'publish',
+        'posts_per_page'   => -1,
+        'caller_get_posts' => 1,
+    );
 
-  // The Query
-  return new WP_Query( array_merge($args, $options) );
+    // The Query
+    return new WP_Query( array_merge($args, $options) );
 }
 
 
@@ -102,7 +102,7 @@ function GetPosts($type, $options=[])
 */
 function getFields($field = '')
 {
-  return get_post_custom_values( $field );
+    return get_post_custom_values( $field );
 }
 
 
@@ -113,7 +113,7 @@ function getFields($field = '')
 */
 function getField($field = '')
 { 
-  return array_pop(get_post_custom_values( $field ));
+    return array_pop(get_post_custom_values( $field ));
 }
 
 
@@ -124,29 +124,51 @@ function getField($field = '')
 */
 function getImgLink($field='')
 {
-  return array_shift(wp_get_attachment_image_src(getField( $field ), 'full'));
+    return array_shift(wp_get_attachment_image_src(getField( $field ), 'full'));
 }
 
 
 
-
-
-if ( !function_exists( 'getOption' ) ) {
+/*
+|------------------------------------------------------------------------------------
+| Get option of Theme
+|------------------------------------------------------------------------------------
+*/
 function getOption($name, $default = false) {
 
-  $optionsframework_settings = get_option('optionsframework');
+    $optionsframework_settings = get_option('optionsframework');
 
-  // Gets the unique option id
-  $option_name = $optionsframework_settings['id'];
+    // Gets the unique option id
+    $option_name = $optionsframework_settings['id'];
 
-  if ( get_option($option_name) ) {
-    $options = get_option($option_name);
-  }
+    if ( get_option($option_name) ) {
+        $options = get_option($option_name);
+    }
 
-  if ( isset($options[$name]) ) {
-    return $options[$name];
-  } else {
-    return $default;
-  }
+    if ( isset($options[$name]) ) {
+        return $options[$name];
+    } else {
+        return $default;
+    }
 }
+
+
+/*
+|------------------------------------------------------------------------------------
+| Cherif get Post type
+|------------------------------------------------------------------------------------
+*/
+function GetCPT($cpt='')
+{
+    global $wpdb;
+    $sql = "
+            SELECT * 
+            FROM $wpdb->posts p 
+            LEFT JOIN {$wpdb->prefix}postmeta m ON p.ID=m.post_id  
+            WHERE p.post_type = 'slide'
+        ";
+
+    return $wpdb->get_results($sql);
+
 }
+
--- inc/home-slider.php
+++ inc/home-slider.php
@@ -12,23 +12,23 @@ $i     = 0;
           <div class="container">
             <div class="carousel-inner" role="listbox">
               <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-
                 <div class="item <?php echo ($i == 0) ? 'active' : '' ?>">
                   <div class="row">
                     <div class="col-md-7 hidden-xs">
                       <img src="<?php echo getImgLink('image_slide') ?>" alt="">
                     </div>
                     <div class="col-md-5">
-                      <h2><?php echo getField( 'sub_title' ) ?></h2>
+                      <h2><?php the_content(); ?></h2>
                       <div class="content">
                         <?php if (getField('link_video')): ?>
-                          <a class="video" href="<?php echo getField('link_video') ?>"><img src="wp-content/uploads/2015/02/video.png" alt=""></a>
+                          <a class="video" href="<?php echo getField('link_video') ?>">
+                            <img src="<?php echo getImgLink('image_video') ?>" alt="">
+                          </a>
                         <?php endif ?>
-                        <p><strong><?php echo the_content(); ?></strong></p>
+                        <p><strong><?php echo getField( 'slider_subtitle' ) ?></strong></p>
                       </div>
-                      <a href="<?php echo getField('text_link') ?>" class="btn btn-danger">Get started</a>
+                      <a href="<?php echo getField('button_link') ?>" class="btn btn-danger"><?php echo getField('button_text') ?></a>
                     </div>
-
                   </div>
                 </div>
 
@@ -38,80 +38,19 @@ $i     = 0;
               <!-- no posts found -->
               <?php endif; ?>
             
-              <!-- <div class="item">
-                <div class="row">
-                  <div class="col-md-7 hidden-xs">
-                    <img src="img/slide-01.png" alt="">
-                  </div>
-                  <div class="col-md-5">
-                    <h2>Timed, Scripted, Standards-Based Lessons Exploring 35 Master Artists</h2>
-                    <div class="content">
-                      <a class="video" href="#"><img src="img/video.png" alt=""></a>
-                      <p><strong>See pricing and get a sneak peek video of the Leonardo da Vinci lesson!</strong></p>
-                    </div>
-                    <a href="#" class="btn btn-danger">Get started</a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="item">
-                <div class="row">
-                  <div class="col-md-7 hidden-xs">
-                    <img src="img/slide-01.png" alt="">
-                  </div>
-                  <div class="col-md-5">
-                    <h2>Timed, Scripted, Standards-Based Lessons Exploring 35 Master Artists</h2>
-                    <div class="content">
-                      <a class="video" href="#"><img src="img/video.png" alt=""></a>
-                      <p><strong>See pricing and get a sneak peek video of the Leonardo da Vinci lesson!</strong></p>
-                    </div>
-                    <a href="#" class="btn btn-danger">Get started</a>
-                  </div>
-                </div>
-              </div>
-
-              <div class="item">
-                <div class="row">
-                  <div class="col-md-7 hidden-xs">
-                    <img src="img/slide-01.png" alt="">
-                  </div>
-                  <div class="col-md-5">
-                    <h2>Timed, Scripted, Standards-Based Lessons Exploring 35 Master Artists</h2>
-                    <div class="content">
-                      <a class="video" href="#"><img src="img/video.png" alt=""></a>
-                      <p><strong>See pricing and get a sneak peek video of the Leonardo da Vinci lesson!</strong></p>
-                    </div>
-                    <a href="#" class="btn btn-danger">Get started</a>
-                  </div>
-                </div>
-              </div>
-            
-              <div class="item">
-                <div class="row">
-                  <div class="col-md-7 hidden-xs">
-                    <img src="img/slide-01.png" alt="">
-                  </div>
-                  <div class="col-md-5">
-                    <h2>Timed, Scripted, Standards-Based Lessons Exploring 35 Master Artists</h2>
-                    <div class="content">
-                      <a class="video" href="#"><img src="img/video.png" alt=""></a>
-                      <p><strong>See pricing and get a sneak peek video of the Leonardo da Vinci lesson!</strong></p>
-                    </div>
-                    <a href="#" class="btn btn-danger">Get started</a>
-                  </div>
-                </div>
-              </div> -->
-            
             </div><!-- carousel-inner -->
           </div>
         
           <div class="navig">
             <ol class="carousel-indicators list-inline">
-              <li data-target="#carousel-example-generic" data-slide-to="0" class="active">How it Works</li>
-              <li data-target="#carousel-example-generic" data-slide-to="1">What is MTM?</li>
-              <li data-target="#carousel-example-generic" data-slide-to="2">35 Artists, 5 Tracks</li>
-              <li data-target="#carousel-example-generic" data-slide-to="3">The MTM Story</li>
-              <li data-target="#carousel-example-generic" data-slide-to="4">Sneak Peek</li>
+              <?php $i = 0; if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+                <li data-target="#carousel-example-generic" data-slide-to="<?php echo $i ?>" <?php echo ($i == 0) ? 'class="active"' : '' ?>><?php echo the_title(); ?></li>
+              <?php $i++; endwhile; ?>
+              <!-- post navigation -->
+              <?php else: ?>
+              <!-- no posts found -->
+              <?php endif; ?>
+
             </ol>
           </div>
 
