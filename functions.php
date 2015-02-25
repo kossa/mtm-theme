11ac114d58217c5e563ee593ade441733add3d55
kossa <hadjikouceyla@gmail.com>
2015-02-25 08:52:49 +0100

    just commit

--- functions.php
+++ functions.php
@@ -2,8 +2,10 @@
 
 session_start();
 
-/**
-    For debuggin
+/*
+|------------------------------------------------------------------------------------
+| For debuggin
+|------------------------------------------------------------------------------------
 */
 //var_dump($wpdb);  
 
@@ -14,8 +16,10 @@ function mtm_register_theme_menu() {
 }
 add_action( 'init', 'mtm_register_theme_menu' );
 
-/**
-    Add Widget
+/*
+|------------------------------------------------------------------------------------
+| Add Widget
+|------------------------------------------------------------------------------------
 */
 function mtm_widgets_init() {
 
@@ -40,21 +44,27 @@ function mtm_widgets_init() {
 }
 add_action( 'widgets_init', 'mtm_widgets_init' );
 
-/**
-    Add support Thumblnails
-    */
+/*
+|------------------------------------------------------------------------------------
+| Add support Thumblnails
+|------------------------------------------------------------------------------------
+*/
 add_theme_support( 'post-thumbnails' ); 
 
 
-/**
-    Remove filter of <p></p>
+/*
+|------------------------------------------------------------------------------------
+| Remove filter of <p></p>
+|------------------------------------------------------------------------------------
 */
 remove_filter( 'the_content', 'wpautop' );
 remove_filter( 'the_excerpt', 'wpautop' );
 
 
-/**
-    my classes in home page
+/*
+|------------------------------------------------------------------------------------
+| my classes in home page
+|------------------------------------------------------------------------------------
 */
 function add_class_body(){
     $classes = array();
@@ -63,250 +73,61 @@ function add_class_body(){
     }
 
     return $classes;
-}   
-
-/**
-    Get All categories as Object (Categories of winetechri)
-*/
-function getProductsObject(){
-    $args = array(
-        'child_of'                 => 0,
-        'parent'                   => 0,
-        'orderby'                  => 'ID',
-        'order'                    => 'ASC',
-        'hide_empty'               => 0,
-        'hierarchical'             => 0,
-        'exclude'                  => '',
-        'include'                  => '',
-        'taxonomy'                 => 'produits',
-        'pad_counts'               => false
-    );
-
-    return 
-        get_categories( $args );
-}   
-
-
-/**
-    Get All categories as an Array (Categories of winetechri)
-*/
-function    getProductsArray(){
-    $allCat = getProductsObject();
-    $result = array();
-
-    foreach ($allCat as $key) {
-        array_push($result, $key->name);
-    }
-    return $result;
-}
-
-/**
-    Get all products of one category
-*/
-function getAllProducts($productName){
-    $arg = array(
-      'post_type'        =>  'mtm',
-      'post_status'      => 'publish',
-      'posts_per_page'   => -1,
-      'caller_get_posts' => 1,
-      'produits'         => $productName,
-  );
-
-
-  return new WP_Query($arg);
-}
-
-/**
-    Get Regions List
-*/
-function getRegionsList(){
-    $regions = array();
-    $args = array(
-    'type'                     => 'mtm',
-    'orderby'                  => 'name',
-    'hide_empty'               => 0,
-    'taxonomy'                 => 'regions',
-  ); 
-
-  $allcat = get_categories( $args );
-
-  foreach ($allcat as $onecat) {
-    $regions[$onecat->slug] = $onecat->name;
-  }
-  return $regions;
-}   
-
-/**
-  getProductDetails
-*/
-function getProductDetails(){
-
-  $ID = get_the_id();
-  $obj = new stdClass();
-  $x = array_pop(get_post_custom_values("image_produit"));
-
-  $obj->title         = get_the_title();
-  $obj->date          = get_the_date( "d/m/Y" );
-  $obj->prix          = array_pop( get_post_custom_values( "prix" ) );
-  $obj->content       = get_the_content();
-  $obj->adresse       = array_pop(get_post_custom_values("adresse"));
-  $obj->telephone     = array_pop(get_post_custom_values("telephone"));
-  $obj->map           = array_pop(get_post_custom_values("map"));
-  $obj->image_produit = wp_get_attachment_image_src($x, "full")[0];
-
-  if(isset($_SESSION[$ID]) == false){
-
-    // Hits
-    $hits = 0;
-    $hits = get_post_custom_values("hits")[0];
-
-    if($hits == 0){
-      $newhits = 1;
-      add_post_meta($ID, "hits", $newhits);
-    }else{
-      $newhits = $hits + 1;
-      update_post_meta($ID, "hits", $newhits);
-    }
-    $_SESSION[$ID] = $newhits;
-
-  }else{
-    echo "Rani hna";
-    $newhits = $_SESSION[$ID];
-  }
-
-  $obj->hits = $newhits;
-
-  return $obj;
-}
-
-
-/**
-  Get all products of slider in home page
-*/
-function getProductsSlider($productName = null){
-  $arg = array(
-      'post_type'        =>  'mtm',
-      'post_status'      => 'publish',
-      'posts_per_page'   => -1,
-      'caller_get_posts' => 1,
-      
-
-  );
-
-  if($productName == null){ // Slide 
-    $arg['meta_query'] = array(
-          array(
-            'key'     => 'status',
-            'value'   => 'home',
-            'type'    => 'CHAR',
-            'compare' => 'LIKE'
-          ),
-      );
-  }else{ // Slide for categories
-    $arg['produits'] = $productName;
-
-    $arg['meta_query'] = array(
-          array(
-            'key'     => 'status',
-            'value'   => 'list',
-            'type'    => 'CHAR',
-            'compare' => 'LIKE'
-          ),
-      );
-  }
-
-
-  return new WP_Query($arg);
-}
-
-/**
-  Get product with search
-*/
-function getSearch($name = null, $category = null, $produits = null, $region = null, $prix_min = null, $prix_max){
-    $arg = array(
-      'post_type'        =>  'mtm',
-      'post_status'      => 'publish',
-      'orderby'          => 'ID',
-      'order'            => 'DESC'
-    );
-
-
-    // Set Product category
-    if ($produits != null) {
-      $arg['tax_query'] = array(
-        array(
-          'taxonomy' => 'produits',
-          'field' => 'id',
-          'terms' => $produits
-          ) );
-    }else{
-      if ($category != null) {
-        $arg['tax_query'] = array(
-        array(
-          'taxonomy' => 'produits',
-          'field' => 'id',
-          'terms' => $category
-          ));
-      }
-    }
-
-    $arg['meta_query'] = array();
-
-    // Set Region
-    if ($region) {
-      array_push($arg['meta_query'], array(
-        'key' => 'region',
-        'value' => $region,
-        'type' => 'CHAR',
-        'compare' => '='
-      ));
-    }
-
-    // Set prix min
-    if ($prix_min) {
-      array_push($arg['meta_query'], array(
-        'key' => 'prix',
-        'value' => $prix_min,
-        'type' => 'NUMERIC',
-        'compare' => '>='
-      ));
-    }
-
-    // Set prix max
-    if ($prix_max) {
-      array_push($arg['meta_query'], array(
-        'key' => 'prix',
-        'value' => $prix_max,
-        'type' => 'NUMERIC',
-        'compare' => '<='
-      ));
-    }
-
-  $wp_query = new WP_Query($arg);
-  return $wp_query;  
-
 }  
 
-/**
-  Get posts with type
-*/
-function getTypeProduct($type){
 
-  $arg = array(
-    'post_type'        =>  'mtm',
+/*
+|------------------------------------------------------------------------------------
+| Get Posts
+|------------------------------------------------------------------------------------
+*/
+function GetPosts($type, $options=[])
+{
+  $args = array (
+    'post_type'        => $type,
     'post_status'      => 'publish',
     'posts_per_page'   => -1,
     'caller_get_posts' => 1,
-    'meta_query'       => array(
-          array(
-            'key'     => 'type_produit',
-            'value'   => $type,
-            'type'    => 'CHAR',
-            'compare' => 'LIKE'
-          ),
-      )
   );
 
-  return new WP_Query($arg);
-}  
+  // The Query
+  return new WP_Query( array_merge($args, $options) );
+}
+
+
+/*
+|------------------------------------------------------------------------------------
+| Get All fields
+|------------------------------------------------------------------------------------
+*/
+function getFields($field = '')
+{
+  return get_post_custom_values( $field );
+}
+
+
+/*
+|------------------------------------------------------------------------------------
+| Get Field (just the first)
+|------------------------------------------------------------------------------------
+*/
+function getField($field = '')
+{ 
+  return array_pop(get_post_custom_values( $field ));
+}
+
+
+/*
+|------------------------------------------------------------------------------------
+| Get Field as an image
+|------------------------------------------------------------------------------------
+*/
+function getImgLink($field='')
+{
+  return array_shift(wp_get_attachment_image_src(getField( $field ), 'full'));
+}
+
+
+
+
 
--- inc/home-slider.php
+++ inc/home-slider.php
@@ -1,26 +1,44 @@
+<?php 
+
+$query = GetPosts('slide');
+$i     = 0;
+
+?>
+
 <section class="slider">
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
           
           <!-- Wrapper for slides -->
           <div class="container">
             <div class="carousel-inner" role="listbox">
-              <div class="item active">
-                <div class="row">
-                  <div class="col-md-7 hidden-xs">
-                    <img src="img/slide-01.png" alt="">
-                  </div>
-                  <div class="col-md-5">
-                    <h2>Timed, Scripted, Standards-Based Lessons Exploring 35 Master Artists</h2>
-                    <div class="content">
-                      <a class="video" href="#"><img src="img/video.png" alt=""></a>
-                      <p><strong>See pricing and get a sneak peek video of the Leonardo da Vinci lesson!</strong></p>
+              <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+
+                <div class="item <?php echo ($i == 0) ? 'active' : '' ?>">
+                  <div class="row">
+                    <div class="col-md-7 hidden-xs">
+                      <img src="<?php echo getImgLink('image_slide') ?>" alt="">
                     </div>
-                    <a href="#" class="btn btn-danger">Get started</a>
+                    <div class="col-md-5">
+                      <h2><?php echo getField( 'sub_title' ) ?></h2>
+                      <div class="content">
+                        <?php if (getField('link_video')): ?>
+                          <a class="video" href="<?php echo getField('link_video') ?>"><img src="wp-content/uploads/2015/02/video.png" alt=""></a>
+                        <?php endif ?>
+                        <p><strong><?php echo the_content(); ?></strong></p>
+                      </div>
+                      <a href="<?php echo getField('text_link') ?>" class="btn btn-danger">Get started</a>
+                    </div>
+
                   </div>
                 </div>
-              </div> <!-- item -->
+
+              <?php $i++; endwhile; ?>
+              <!-- post navigation -->
+              <?php else: ?>
+              <!-- no posts found -->
+              <?php endif; ?>
             
-              <div class="item">
+              <!-- <div class="item">
                 <div class="row">
                   <div class="col-md-7 hidden-xs">
                     <img src="img/slide-01.png" alt="">
@@ -34,7 +52,7 @@
                     <a href="#" class="btn btn-danger">Get started</a>
                   </div>
                 </div>
-              </div> <!-- item -->
+              </div>
 
               <div class="item">
                 <div class="row">
@@ -50,7 +68,7 @@
                     <a href="#" class="btn btn-danger">Get started</a>
                   </div>
                 </div>
-              </div> <!-- item -->
+              </div>
 
               <div class="item">
                 <div class="row">
@@ -66,7 +84,7 @@
                     <a href="#" class="btn btn-danger">Get started</a>
                   </div>
                 </div>
-              </div> <!-- item -->
+              </div>
             
               <div class="item">
                 <div class="row">
@@ -82,7 +100,7 @@
                     <a href="#" class="btn btn-danger">Get started</a>
                   </div>
                 </div>
-              </div> <!-- item -->
+              </div> -->
             
             </div><!-- carousel-inner -->
           </div>
