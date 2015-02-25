938ff7368452926c4cc6af4481550a75ebc85d82
kossa <hadjikouceyla@gmail.com>
2015-02-25 18:41:20 +0100

    Home - Top Ten done

--- inc/top-ten.php
+++ inc/top-ten.php
@@ -1,15 +1,22 @@
+
+<?php $query = GetPosts('top-ten'); ?>
+
 <section class="top-ten">
   <div class="container">
-    <h1>Top 10 Reasons to Choose Meet the Masters Art Lessons</h1>
+    <h1><?php echo getOption('top_ten_title') ?></h1>
     <ul class="list-inline">
-      <li><a href="#"><img src="img/img-01.png" alt=""><span>Over 3 Million Artists Taught</span></a></li>
-      <li><a href="#"><img src="img/img-02.png" alt=""><span>Over 3 Million Artists Taught</span></a></li>
-      <li><a href="#"><img src="img/img-03.png" alt=""><span>Easy to Implement</span></a></li>
-      <li><a href="#"><img src="img/img-04.png" alt=""><span>Age Appropriate with 4 Levels</span></a></li>
-      <li><a href="#"><img src="img/img-05.png" alt=""><span>Teaches Art History and Art Activities</span></a></li>
-      <li><a href="#"><img src="img/img-06.png" alt=""><span>Art Supply Kits Available</span></a></li>
+      <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+        <li><a><img src="<?php echo getImgLink('icon') ?>" alt=""><span><?php the_title(); ?></span></a></li>
+      <?php endwhile; ?>
+      <!-- post navigation -->
+      <?php else: ?>
+      <!-- no posts found -->
+      <?php endif; ?>
+      
     </ul>
 
-    <a href="#" class="btn btn-danger">Read more reasons</a>
+    <a href="<?php echo getOption('top_ten_link') ?>" class="btn btn-danger">
+      <?php echo getOption('top_ten_text') ?>
+    </a>
   </div>
 </section> <!-- top-ten -->
--- options.php
+++ options.php
@@ -26,74 +26,7 @@ function optionsframework_option_name() {
 
 function optionsframework_options() {
 
-	// Test data
-	$test_array = array(
-		'one' => __('One', 'options_check'),
-		'two' => __('Two', 'options_check'),
-		'three' => __('Three', 'options_check'),
-		'four' => __('Four', 'options_check'),
-		'five' => __('Five', 'options_check')
-	);
-
-	// Multicheck Array
-	$multicheck_array = array(
-		'one' => __('French Toast', 'options_check'),
-		'two' => __('Pancake', 'options_check'),
-		'three' => __('Omelette', 'options_check'),
-		'four' => __('Crepe', 'options_check'),
-		'five' => __('Waffle', 'options_check')
-	);
-
-	// Multicheck Defaults
-	$multicheck_defaults = array(
-		'one' => '1',
-		'five' => '1'
-	);
-
-	// Background Defaults
-	$background_defaults = array(
-		'color' => '',
-		'image' => '',
-		'repeat' => 'repeat',
-		'position' => 'top center',
-		'attachment'=>'scroll' );
-
-	// Typography Defaults
-	$typography_defaults = array(
-		'size' => '15px',
-		'face' => 'georgia',
-		'style' => 'bold',
-		'color' => '#bada55' );
-
-	// Typography Options
-	$typography_options = array(
-		'sizes' => array( '6','12','14','16','20' ),
-		'faces' => array( 'Helvetica Neue' => 'Helvetica Neue','Arial' => 'Arial' ),
-		'styles' => array( 'normal' => 'Normal','bold' => 'Bold' ),
-		'color' => false
-	);
-
-	// Pull all the categories into an array
-	$options_categories = array();
-	$options_categories_obj = get_categories();
-	foreach ($options_categories_obj as $category) {
-		$options_categories[$category->cat_ID] = $category->cat_name;
-	}
-
-	// Pull all tags into an array
-	$options_tags = array();
-	$options_tags_obj = get_tags();
-	foreach ( $options_tags_obj as $tag ) {
-		$options_tags[$tag->term_id] = $tag->name;
-	}
-
-	// Pull all the pages into an array
-	$options_pages = array();
-	$options_pages_obj = get_pages('sort_column=post_parent,menu_order');
-	$options_pages[''] = 'Select a page:';
-	foreach ($options_pages_obj as $page) {
-		$options_pages[$page->ID] = $page->post_title;
-	}
+	
 
 	// If using image radio buttons, define a directory path
 	$imagepath =  get_template_directory_uri() . '/images/';
@@ -101,7 +34,7 @@ function optionsframework_options() {
 	$options = array();
 
 	$options[] = array(
-		'name' => __('Basic Settings', 'options_check'),
+		'name' => __('Header', 'options_check'),
 		'type' => 'heading');
 
 	$options[] = array(
@@ -137,6 +70,27 @@ function optionsframework_options() {
 		'id' => 'pinterest_url',
 		'type' => 'text');
 
+	$options[] = array(
+		'name' => __('Sections', 'options_check'),
+		'type' => 'heading');
+
+	$options[] = array(
+		'name' => __('Top 10 Title', 'options_check'),
+		'id' => 'top_ten_title',
+		'std' => 'Top 10 Reasons to Choose Meet the Masters Art Lessons',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __('Top 10 Button Text', 'options_check'),
+		'id' => 'top_ten_text',
+		'std' => 'Read more reasons',
+		'type' => 'text');
+
+	$options[] = array(
+		'name' => __('Top 10 Button Link', 'options_check'),
+		'id' => 'top_ten_link',
+		'std' => '#',
+		'type' => 'text');
 
 
 
