652b42334c430ebfb01d87daedcaa6994b846159
kossa <hadjikouceyla@gmail.com>
2015-02-26 07:07:25 +0100

    Newsletter Done

--- inc/news-letter.php
+++ inc/news-letter.php
@@ -1,12 +1,5 @@
 <section class="news-letter">
   <div class="container">
-    <form action="#">
-      <ul class="list-inline">
-        <li><h2>sign up for our newsletter</h2></li>
-        <li><input type="text" name="" value="" placeholder="Name"></li>
-        <li><input type="text" name="" value="" placeholder="Email"></li>
-        <li><input type="submit" name="" value="Submit" class="btn btn-danger"></li>
-      </ul>
-    </form>
+    <?php the_widget( "NewsletterWidget" ); ?>
   </div>
 </section><!-- news-latter -->
