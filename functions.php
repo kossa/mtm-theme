f634f55fba825bdacd61dd1033146f2169e6759e
kossa <hadjikouceyla@gmail.com>
2015-03-05 14:59:37 +0100

    Add blog

--- functions.php
+++ functions.php
@@ -212,3 +212,27 @@ function GetValuesMeta($key='')
 
     return $result;
 }
+
+
+/*
+|------------------------------------------------------------------------------------
+| Pagination
+|------------------------------------------------------------------------------------
+*/
+add_filter( 'wp_pagenavi', 'ik_pagination', 10, 2 );
+  
+//customize the PageNavi HTML before it is output
+function ik_pagination($html) {
+    $out = '';
+  
+    //wrap a's and span's in li's
+    $out = str_replace("<div","",$html);
+    $out = str_replace("class='wp-pagenavi'>","",$out);
+    $out = str_replace("<a","<li><a",$out);
+    $out = str_replace("</a>","</a></li>",$out);
+    $out = str_replace("<span","<li><span",$out);  
+    $out = str_replace("</span>","</span></li>",$out);
+    $out = str_replace("</div>","",$out);
+  
+    return '<div class="text-center"><ul class="pagination">'.$out.'</ul></div>';
+}
